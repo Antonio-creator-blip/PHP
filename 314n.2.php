b3ae711b9d3c1f2aecf3107b2f4bdf0e6e0d5750
Antonio-Crisci <72600111+Antonio-creator-blip@users.noreply.github.com>
2022-01-03 21:07:06 +0100

    Add files via upload

---
+++ 314n.2.php
@@ -0,0 +1,43 @@
+<?php 
+  if($_POST){
+      
+        if(isset($_COOKIE['nome'])){
+            echo "Nome: ".$_COOKIE['nome'] ."<br>";
+            echo "Cognome: ".$_COOKIE['cognome'];
+        }
+
+        else{
+            $nome = $_POST['nome'] ;
+            $cognome = $_POST['cognome'];
+            echo "Nome : ".$nome ."<br>";
+            echo "Cognome : ".$cognome;
+            
+        }
+
+        setcookie('nome', $_POST['nome'], time()+10);
+        setcookie('cognome', $_POST['cognome'], time()+10);
+      
+
+        
+      
+  }
+  
+  else
+    {
+    echo "<FORM ACTION= " . $_SERVER['PHP_SELF'] ." METHOD='POST'>";
+    ?>
+    
+    <TABLE >
+            <TR>
+                <TD>Nome: <INPUT type="text" NAME="nome"></TD> 
+            </TR><br>
+            <TR>
+                <TD>Cognome : <INPUT type="text" NAME="cognome"></TD> 
+            </TR><br>
+        </TABLE>
+        <INPUT type="submit" value="Invia">
+        </FORM>
+    <?php
+    }
+    
+    ?>
